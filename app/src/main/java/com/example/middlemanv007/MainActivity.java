78cd40b77ae814aab779f69dd6a7bf8d61f9e479
Gokul41602 <gokul.18it@kct.ac.in>
2021-06-30 09:25:45 +0530

    login authentication successful

--- app/src/main/java/com/example/middlemanv007/MainActivity.java
+++ app/src/main/java/com/example/middlemanv007/MainActivity.java
@@ -1,39 +1,109 @@
 package com.example.middlemanv007;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     TextInputLayout userName, password;
     Button loginButton;
     String userNameText, passwordText;
+    RelativeLayout relativeLayout;
     TextView newUserButton;
+    DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeViews();
-        initializeStrings();
+
 
         newUserButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, UserRegistration.class);
-                startActivity(intent);
-                MainActivity.this.finish();
+
+               Intent intent=new Intent(MainActivity.this,UserRegistration.class);
+               startActivity(intent);
+               MainActivity.this.finish();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                initializeStrings();
+                validateUserName();
             }
         });
     }
 
+    private void validateUserName()
+    {
+        reference= FirebaseDatabase.getInstance().getReference("UsersDto");
+
+        Query query=reference.orderByChild("userName").startAt(userNameText).endAt(userNameText+"\uf8ff");
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists())
+                {
+                    String userNameFromDB=snapshot.child(userNameText).child("userName").getValue(String.class);
+                    if(userNameText.equals(userNameFromDB))
+                    {
+                        userName.setError(null);
+                        userName.setErrorEnabled(true);
+                        String passwordFromDB=snapshot.child(userNameText).child("password").getValue(String.class);
+                        if(passwordText.equals(passwordFromDB))
+                        {
+                            password.setErrorEnabled(false);
+                            password.setError(null);
+                            Snackbar.make(relativeLayout,"Login Successfull",Snackbar.LENGTH_SHORT).show();
+                        }
+                        else
+                        {
+                            password.setErrorEnabled(true);
+                            password.setError("Incorrect Password");
+                            password.requestFocus();
+                        }
+                    }
+                    else
+                    {
+                        userName.setError("User name doesn't exists");
+                        userName.setErrorEnabled(true);
+                        userName.requestFocus();
+                    }
+                }
+                else {
+                    Log.d("failed","data doesn't exists");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
     private void initializeStrings() {
         userNameText = userName.getEditText().getText().toString();
         passwordText = password.getEditText().getText().toString();
@@ -43,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         userName = findViewById(R.id.loginPage_userName);
         password = findViewById(R.id.loginPage_password);
         loginButton = findViewById(R.id.loginPage_loginButton);
+        relativeLayout=findViewById(R.id.mainActivity_relLayout);
         newUserButton = findViewById(R.id.loginPage_newUserButton);
     }
 }
