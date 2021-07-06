c635ddd37ddfbd5b1c4f1f6aa68465098ddd2dbf
Gokul41602 <gokul.18it@kct.ac.in>
2021-07-06 12:51:06 +0530

    changed name for all activities

--- app/src/main/java/com/example/middlemanv007/ui/company/companyFragment.java
+++ app/src/main/java/com/example/middlemanv007/ui/company/companyFragment.java
@@ -1,61 +1,50 @@
-package com.example.middlemanv007.ui.slideshow;
+package com.example.middlemanv007.ui.company;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.middlemanv007.AuthorizedCompanyDto;
-import com.example.middlemanv007.AuthorizedVendorDto;
-import com.example.middlemanv007.CompanyRecyclerAdapter;
 import com.example.middlemanv007.R;
-import com.example.middlemanv007.VendorRecyclerAdapter;
+import com.example.middlemanv007.CompanyRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class SlideshowFragment extends Fragment {
+public class companyFragment extends Fragment {
 
     private View root;
     private RecyclerView recyclerView;
-    private VendorRecyclerAdapter adapter;
-
+    private CompanyRecyclerAdapter adapter;
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-         root = inflater.inflate(R.layout.fragment_slideshow, container, false);
-
-        recyclerView=root.findViewById(R.id.vendor_recView);
+        root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        recyclerView=root.findViewById(R.id.company_recView);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-        FirebaseRecyclerOptions<AuthorizedVendorDto> options
-                = new FirebaseRecyclerOptions.Builder<AuthorizedVendorDto>()
-                .setQuery(FirebaseDatabase.getInstance().getReference("Vendor"), AuthorizedVendorDto.class)
+        FirebaseRecyclerOptions<AuthorizedCompanyDto> options
+                = new FirebaseRecyclerOptions.Builder<AuthorizedCompanyDto>()
+                .setQuery(FirebaseDatabase.getInstance().getReference("Company"), AuthorizedCompanyDto.class)
                 .build();
-        adapter = new VendorRecyclerAdapter(options, root.getContext());
+        adapter = new CompanyRecyclerAdapter(options, root.getContext());
         recyclerView.setAdapter(adapter);
 
         recyclerView.setNestedScrollingEnabled(false);
         return root;
     }
-
     @Override
     public void onStart() {
         super.onStart();
         adapter.startListening();
     }
-
     @Override
     public void onStop() {
         super.onStop();
         adapter.stopListening();
     }
-
 }
--- app/src/main/java/com/example/middlemanv007/ui/vendor/vendorFragment.java
+++ app/src/main/java/com/example/middlemanv007/ui/vendor/vendorFragment.java
@@ -1,4 +1,4 @@
-package com.example.middlemanv007.ui.gallery;
+package com.example.middlemanv007.ui.vendor;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,41 +10,46 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.middlemanv007.AuthorizedCompanyDto;
+import com.example.middlemanv007.AuthorizedVendorDto;
 import com.example.middlemanv007.R;
-import com.example.middlemanv007.CompanyRecyclerAdapter;
+import com.example.middlemanv007.VendorRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class GalleryFragment extends Fragment {
+public class vendorFragment extends Fragment {
 
     private View root;
     private RecyclerView recyclerView;
-    private CompanyRecyclerAdapter adapter;
+    private VendorRecyclerAdapter adapter;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        recyclerView=root.findViewById(R.id.company_recView);
+         root = inflater.inflate(R.layout.fragment_slideshow, container, false);
+
+        recyclerView=root.findViewById(R.id.vendor_recView);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(root.getContext()));
-        FirebaseRecyclerOptions<AuthorizedCompanyDto> options
-                = new FirebaseRecyclerOptions.Builder<AuthorizedCompanyDto>()
-                .setQuery(FirebaseDatabase.getInstance().getReference("Company"), AuthorizedCompanyDto.class)
+        FirebaseRecyclerOptions<AuthorizedVendorDto> options
+                = new FirebaseRecyclerOptions.Builder<AuthorizedVendorDto>()
+                .setQuery(FirebaseDatabase.getInstance().getReference("Vendor"), AuthorizedVendorDto.class)
                 .build();
-        adapter = new CompanyRecyclerAdapter(options, root.getContext());
+        adapter = new VendorRecyclerAdapter(options, root.getContext());
         recyclerView.setAdapter(adapter);
 
         recyclerView.setNestedScrollingEnabled(false);
         return root;
     }
+
     @Override
     public void onStart() {
         super.onStart();
         adapter.startListening();
     }
+
     @Override
     public void onStop() {
         super.onStop();
         adapter.stopListening();
     }
+
 }
